7e33d90c3210bd1f0b7a5f1997a9b3dd9eaad127
Shalom Orangi <shalom.orangi@student.moringaschool.com>
2023-12-07 12:27:32 +0300

    update

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_num= [ n for n in num_list if n %2 ==0]
+    print(even_num)
+    
+return_evens([0, 1, 3, 5, 7, 8, 9])
+
 
 def make_exclamation(sentence_list):
-    pass
+    exclamate=[item + "!" for item in sentence_list]
+    print (exclamate)
+
+make_exclamation(["Hello", "I'm doing great", "Python is fun"])
